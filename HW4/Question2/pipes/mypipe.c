a114594031357c700356141a56fd6e286aad22f6
omraheja <om.raheja@colorado.edu>
2019-03-01 12:53:57 -0700

    adding pipes

--- HW4/Question2/pipes/mypipe.c
+++ HW4/Question2/pipes/mypipe.c
@@ -5,106 +5,119 @@
 	@References	: https://www.youtube.com/watch?v=7bNMkvcOKlc
 */
 
+/* Standard C library Headers */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+/* Led States */
+bool LED_ON;
+bool LED_OFF;
+
 typedef struct{
-	char *string[10];
+	char *string;
 	int string_length;
 	int led_control_command;
 }msg_t;
 
 
+/* File pointer */
+FILE *fileptr;
+
+
 /* Array of string for populating the structure */
+char *array[]={"Message0","Message1","Message2","Message3","Message4"};
 char *array_for_parent[]={"parent0","parent1","parent2","parent3","parent4","parent5","parent6","parent7","parent8","parent9"};
 char *array_for_child[]={"child0","child1","child2","child3","child4","child5","child6","child7","child8","child9"};
 
-/* Standard C library Headers */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
 int main()
 {
-	/* To store pid number of new child */
+	int pipe1[2]; //parent to write, child to read
+	int pipe2[2]; //child to write, parent to read
+	int returnstatus1; //check pipe()
+	int returnstatus2; //check pipe()
 	pid_t pid;
+	
+	char readmsg[20];
+	char buffer[20];
 
-	/* Check return value of pipe() */
-	int ret_val;
+	msg_t payload;
+	msg_t info;
 
-	char read_msg_parent[20];
-	char read_msg_child[20];
+	returnstatus1 = pipe(pipe1);
+	printf("returnstatus1->%d\n",returnstatus1);
 
-	/* Create instance of msg_t */
-	msg_t msgSend;
-
-	/*  Parent->Child
-		Parents Writes to Child
-		Child Reads
-	 */
-	int mypipe1[2];
-
-	/*  Child->Parent
-		Child Writes to Parent
-		Parent Reads
-	 */
-	int mypipe2[2];
-
-	ret_val = pipe(mypipe1);
-
-	if(ret_val == -1)
+	if(returnstatus1 == -1)
 	{
-		perror("Pipe Error");
-		exit(1);
+		printf("Pipe 1 could not be created\n");
+		return 1;
 	}
 
-	ret_val = pipe(mypipe2);
 
-	if(ret_val == -1)
+	returnstatus2 = pipe(pipe2);
+	printf("returnstatus2->%d\n",returnstatus2);
+	if(returnstatus2 == -1)
 	{
-		perror("Pipe Error");
-		exit(1);
+		printf("Pipe 2 could not be created\n");
+		return 1;
 	}
 
-	/* Create new child */
-	pid = fork();
 
+	pid=fork();
+	printf("pid--> %d\n",pid);
 
-	/* Child Process */
-	if(pid == 0)
+	if(pid<0)
 	{
-
-		printf("Reading from Parent\n");
-		while((read(mypipe1[0],read_msg_child,sizeof(read_msg_child))))
-		{
-			printf("Message : %s\n",read_msg_child);
-		}
-
+		printf("Fork Failed\n");
+		return 1;
+	}
+	else if (pid == 0)			//child process
+	{
+		printf("Writing from child to parent\n");
+		/* Writing to Parent */
 		for(int i=0;i<10;i++)
-		{	
-			msgSend.string[i]= array_for_child[i];
-			write(mypipe2[1],msgSend.string[i],sizeof(msgSend.string[i]));
-			sleep(1);
-		}
-
-	}
-	/* Parent Process */
-	else
-	{
-		printf("Sending messages from parent to child\n");
-		for(int i=0;i<10;i++)
-		{	
-			msgSend.string[i]= array_for_parent[i];
-			write(mypipe1[1],msgSend.string[i],sizeof(msgSend.string[i]));
-			sleep(1);
-		}
-
-		while((read(mypipe2[0],read_msg_parent,sizeof(read_msg_parent))))
 		{
-			printf("Message : %s\n",read_msg_parent);
+			payload.string = array_for_parent[i];
+			write(pipe2[1],payload.string,sizeof(readmsg));
+//			write(pipe2[1],"Hello Parent!",sizeof(readmsg));
+
 		}
 
-
+		printf("Reading from Child\n");
+		/* Reading from Parent */	
+		for(int i=0;i<10;i++)
+		{
+			memset(buffer,0,sizeof(buffer));
+			read(pipe1[0],&buffer,sizeof(buffer));
+			printf("Message received from Parent: %s\n",buffer);
+		}
+		
 	}
+	else			//parent process
+	{
+		printf("In Parent Process\n");
+		/* Reading from Child */
+		for(int i=0;i<10;i++)
+		{
+			memset(readmsg,0,sizeof(readmsg));
+			read(pipe2[0],&readmsg,sizeof(readmsg));
+			printf("Message Received: %s\n",readmsg);
 
+		}
 
+		printf("Writing to Child\n");
+		/* Writing to Child */
+		for(int i=0;i<10;i++)
+		{
+			info.string = array_for_child[i];
+			write(pipe1[1],info.string,sizeof(buffer));
+			//write(pipe1[1],"Hello Child!",sizeof(buffer));
+		}
+	}
 }
